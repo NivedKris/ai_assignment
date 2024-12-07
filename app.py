d08b2fb9a82aff933045e21f5898d0f0457acf93
Nived Krishna <100478612+NivedKris@users.noreply.github.com>
2024-12-08 00:19:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def ask_json():
 
 
 def start_app():
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=False)
 
 if __name__ == "__main__":
     start_app()
