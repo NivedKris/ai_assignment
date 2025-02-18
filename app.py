faa48720d7443022e581e0b4a9c9b790419a1598
Nived Krishna <100478612+NivedKris@users.noreply.github.com>
2025-02-18 19:37:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -154,4 +154,5 @@ def ask_pdf():
     response = {"answer": result["answer"]}
     return response
 
-app.run(port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=False)
